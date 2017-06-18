e7e9248803a1189e0e40c7fbd2a32cbea5ff8d87
liznomura <lizn@hawaii.edu>
2017-06-17 15:51:30 -1000

    completed

--- vault.js
+++ vault.js
@@ -1,4 +1,30 @@
+/*jshint esversion: 6 */
 'use strict';
-module.exports = function() {
-  
-};
+function VaultGenerator() {
+  let myVault = {};
+  return {
+    setValue : setValue,
+    getValue : getValue
+  };
+
+  function setValue ( key, value ) {
+    myVault[key] = value;
+  }
+
+  function getValue ( key ) {
+    if (key === undefined) {
+      return null;
+    } else if (myVault[key] === undefined) {
+      return null;
+    } else {
+      return myVault[key];
+    }
+  }
+}
+
+module.exports = VaultGenerator;
+
+var vault = VaultGenerator();
+vault.setValue("cat", "meow");
+vault.setValue("dog", "bark");
+console.log(vault.getValue("cat"));
